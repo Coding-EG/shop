99608c3a161e9a2897027e5ce44b186d9e96cef3
Coding-EG <100527795+Coding-EG@users.noreply.github.com>
2022-07-13 09:32:48 +0530

    Add files via upload
    dircect file transfer

---
+++ script.js
@@ -0,0 +1,139 @@
+let productData,productSection;
+let yourData = JSON.parse(localStorage.getItem("yourData")) || [];
+productSection = document.getElementById("product-section");
+fetch("data.json").then(x => x.json()).then(x => {
+   productData = x;
+   getProduct();
+}).catch(error => console.log(error));
+let getProduct = () => {
+    productData.map(x => {
+        let {id,link,name,desc,price} =x;
+         // console.log(id,desc,name,link);
+        /*   console.log(des);
+           console.log(name);
+        */
+       let searchData = yourData.find((x) => x.id === id);
+       productSection.innerHTML+=`<div id="product-container-${id}"class="product-container"><div id="product-img-${id}-container"class="product-img-container"><img src="${link}"alt="${name}"class="product-img"></div>
+       <p id="product-name-${id}"class="product-name">${name}</p>
+       <p class="product-desc">${desc}</p><div class="product-price-container">
+       <p class="product-price">$ ${price}</p><div class="product-quantity-container"><i class="product-minus-button"onclick="itemsDecreasement(${id})">&minus;</i>
+       <span id="${id}"class="product-quantity">${searchData === undefined ? 0 : searchData.item}</span>
+       <i class="product-plus-button"onclick="itemsIncreasement(${id})">&plus;</i>
+       </div>
+       </div>
+       </div>`;
+    })
+    
+};
+
+let adsSlider = () =>{
+   let index,slideImg;
+   index = 0;
+   let slide = () => {
+      slideImg = document.querySelectorAll(".ads-container-img");
+      for (let i = 0; i < slideImg.length; i++) {
+         // console.log(slideImg);
+         slideImg[i].style.display = "none";
+         // slideImg[i].style.right = "-100%";
+      }
+      slideImg[index].style.display= "inline";
+      // slideImg[index].style.right= "0px";
+      if (index >= slideImg.length -1) {
+         index = 0;
+      }else{index++}
+      // console.log(index);
+   };
+   document.getElementById("ads-container").style.height = "calc(100vh - 140px)";
+   slide();
+   setInterval(slide,4000);
+};
+adsSlider();
+
+
+function navToggleFunction() {
+   let navUl = document.querySelector("ul");
+   let navLi = document.querySelectorAll(".nav-link");
+   let navHref = document.querySelectorAll(".nav-target");
+   let navToggle = document.getElementById("nav-toggle");
+       if(window.innerWidth<=575) {
+        navToggle.style.display = "inline";
+        navToggle.onclick = () => {
+           navLi.forEach((x)=> {
+             x.classList.toggle("nav-li-toggle");
+             
+           
+           });
+        };
+        navLi.forEach((x) => {x.classList.add("nav-li-toggle");});
+        navHref.forEach((x) => {x.classList.add("nav-target-toggle");});
+        navUl.style.flexDirection= "column";
+       }
+       else{
+           navToggle.style.display = "none";
+           navLi.forEach((x) => {x.classList.remove("nav-li-toggle");});
+           navHref.forEach((x) => {x.classList.remove("nav-target-toggle");});
+           navUl.style.flexDirection = "row";
+       }
+
+
+   window.addEventListener("resize",navToggleFunction);
+};
+navToggleFunction();
+window.onload = () => {document.body.style.display = "block"};
+
+
+
+function itemsIncreasement(id) {
+    if (yourData.map((x) => x.item).reduce((x, y) => x + y, 0) >= 20) 
+    {alert("add 20 items at a time");
+     return;      
+    };
+    let selectedItem = id;
+    let searchData = yourData.find((x) => x.id === selectedItem);
+
+    if (searchData === undefined) {
+      yourData.push(
+         {
+           id:selectedItem,
+           item:1
+         }
+      )
+    } else if (searchData.item === 10) return;
+    else{
+      searchData.item+=1;
+    }
+    localStorage.setItem("yourData",JSON.stringify(yourData));
+    if (searchData === undefined) {
+      searchData = yourData.find((x) => x.id === selectedItem);
+      updateItemQuantity(selectedItem,searchData);
+    } else{
+      updateItemQuantity(selectedItem,searchData);
+    }
+    cartItemQuantity();
+
+};
+
+function itemsDecreasement(id) {
+   let selectedItem = id;
+    let searchData = yourData.find((x) => x.id === selectedItem);
+    if (searchData === undefined) return;
+    else if (searchData.item === 0) return;
+    else if (searchData.item === 1) {
+      searchData.item -=1;
+      // yourData.pop(searchData);
+      yourData = yourData.filter((x) => x.id !== selectedItem);
+   }
+    else{searchData.item -=1;}
+    localStorage.setItem("yourData",JSON.stringify(yourData));
+    updateItemQuantity(selectedItem,searchData);
+    cartItemQuantity();
+};
+
+function updateItemQuantity(id,searchData) {
+   document.getElementById(`${id}`).innerText = `${searchData.item}`;
+};
+function cartItemQuantity() {
+   let cartItem = document.getElementById("header-cart-num");
+  cartItem.innerText = yourData.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+cartItemQuantity();
---
+++ scriptCart.js
@@ -0,0 +1,137 @@
+let yourData = JSON.parse(localStorage.getItem("yourData")) || [];
+let productData;
+
+window.onload = () => {document.body.style.display = "block";
+fetch("data.json").then(x => x.json()).then(x => {productData = x;
+  cartItems();
+});
+};
+function navToggleFunction() {
+    let navUl = document.querySelector("ul");
+    let navLi = document.querySelectorAll(".nav-link");
+    let navHref = document.querySelectorAll(".nav-target");
+    let navToggle = document.getElementById("nav-toggle");
+        if(window.innerWidth<=575) {
+         navToggle.style.display = "inline";
+         navToggle.onclick = () => {
+            navLi.forEach((x)=> {
+              x.classList.toggle("nav-li-toggle");
+              
+            
+            });
+         };
+         navLi.forEach((x) => {x.classList.add("nav-li-toggle");});
+         navHref.forEach((x) => {x.classList.add("nav-target-toggle");});
+         navUl.style.flexDirection= "column";
+        }
+        else{
+            navToggle.style.display = "none";
+            navLi.forEach((x) => {x.classList.remove("nav-li-toggle");});
+            navHref.forEach((x) => {x.classList.remove("nav-target-toggle");});
+            navUl.style.flexDirection = "row";
+        }
+ 
+ 
+    window.addEventListener("resize",navToggleFunction);
+ };
+ navToggleFunction();
+ 
+//  function cartItemQuantity() {
+//     let cartItem = document.getElementById("header-cart-num");
+//    cartItem.innerText = yourData.map((x) => x.item).reduce((x, y) => x + y, 0);
+//  };
+//  cartItemQuantity(); 
+
+ function cartItems() {
+  let productSection = document.getElementById("product-section");
+  console.log(productData);
+  if (yourData.length == 0) {
+    productSection.innerHTML =`<h1 class="empty-cart-heading">Your Cart Is Empty</h1>`;
+  }
+  console.log(yourData);
+  yourData.map(x => {
+    let searchData = productData.find((y) => x.id === y.id);
+    // let searchItemData = yourData.find((x) => x.id === y.id);
+   let {id,link,name,desc,price} = searchData;
+   productSection.innerHTML += `<div class="item-container"id="item-container-${id}"><span class="item-remove"onclick="removeItem(${id})">x</span><img class="item-img"alt="${name}"src="${link}"><div class="item-detail"><p class="item-name">${name}</p><br><p class ="item-desc">${desc}</p></div><div class="item-quantity-container"><i class="item-minus-button"onclick="itemsDecreasement(${id})">&minus;</i>
+   <span id="${id}"class="item-quantity">${x === undefined ? 0 : x.item}</span>
+   <i class="item-plus-button"onclick="itemsIncreasement(${id})">&plus;</i>
+   </div><p class="item-price"id="item-price-${id}">$ ${price*x.item}</p></div>`;
+
+  })
+  
+ };
+ 
+ function itemsIncreasement(id) {
+  if (yourData.map((x) => x.item).reduce((x, y) => x + y, 0) >= 20) 
+  {alert("add 20 items at a time");
+   return;      
+  };
+  let selectedItem = id;
+  let searchData = yourData.find((x) => x.id === selectedItem);
+
+  // if (searchData === undefined) {
+  //   yourData.push(
+  //      {
+  //        id:selectedItem,
+  //        item:1
+  //      }
+  //   )
+  // } else 
+  if (searchData.item === 10) return;
+  else{
+    // console.log(yourData);
+    searchData.item+=1;
+    // console.log(yourData);
+  }
+  localStorage.setItem("yourData",JSON.stringify(yourData));
+  // if (searchData === undefined) {
+  //   searchData = yourData.find((x) => x.id === selectedItem);
+    // updateItemQuantity(selectedItem,searchData);
+  // } else{
+    updateItemQuantity(selectedItem,searchData);
+  // }
+  cartItemQuantity();
+
+};
+
+function itemsDecreasement(id) {
+  let selectedItem = id;
+   let searchData = yourData.find((x) => x.id === selectedItem);
+   if (searchData.item > 1) {
+    searchData.item-=1;
+    localStorage.setItem("yourData",JSON.stringify(yourData));
+    updateItemQuantity(selectedItem,searchData);
+    cartItemQuantity(); 
+    
+   }
+   else if (searchData.item <=1) {
+    document.getElementById(`item-container-${selectedItem}`).remove();
+    yourData = yourData.filter((x) => x.id !== selectedItem);
+    localStorage.setItem("yourData",JSON.stringify(yourData));
+    cartItemQuantity(); 
+   }
+  
+  
+};
+
+function removeItem(id) {
+  let selectedItem = id;
+  document.getElementById(`item-container-${id}`).remove();
+  yourData = yourData.filter((x) => x.id !== selectedItem);
+  localStorage.setItem("yourData",JSON.stringify(yourData));
+  cartItemQuantity();
+
+}
+
+function updateItemQuantity(id,searchData) {
+  if (document.getElementById(`${id}`) ===undefined) return;
+  document.getElementById(`${id}`).innerText = `${searchData === undefined ? 0 : searchData.item}`;
+  document.getElementById(`item-price-${id}`).innerText = `${searchData.item}`;
+};
+function cartItemQuantity() {
+  let cartItem = document.getElementById("header-cart-num");
+ cartItem.innerText = yourData.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+cartItemQuantity();
+ 
